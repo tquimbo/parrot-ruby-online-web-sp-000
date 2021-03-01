a48aeed8aa240259c43b4f6df28c4eb141caf005
Tim Quimbo <tquimbo1@gmail.com>
2021-03-01 15:39:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-puts "Squawk!"
-"Squawk!"
-end
+# def parrot
+# puts "Squawk!"
+# "Squawk!"
+# end
 
-def Squawk(string)
+def parrot(string)
 string = "Squawk!"
 puts "Squawk!"
 return string
